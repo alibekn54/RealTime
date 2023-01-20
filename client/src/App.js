a2497c0f8e70c762600cc8efba55469caf091e3c
alibekN54 <alibekandanuarbek@gmail.com>
2023-01-20 23:55:21 +0600

    add websocket !!!

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import LongPulling from "./LongPulling";
 import EventSourcing from "./EventSourcing";
+import WebSockets from "./WebSockets";
 
 function App() {
   return (
       <div>
-          <LongPulling />
+          <WebSockets />
       </div>
   )
 }
--- client/src/WebSockets.js
+++ client/src/WebSockets.js
@@ -5,27 +5,62 @@ const WebSockets = () => {
     const [messages, setMessages] = useState([]);
     const [value, setValue] = useState('');
     const socket = useRef()
+    const [connected, setConnected] = useState(false);
+    const [username, setUsername] = useState('')
 
-    useEffect( () => {
+
+    function connect() {
         socket.current = new WebSocket('ws://localhost:5000')
 
         socket.current.onopen = () => {
-
+            setConnected(true)
+            const message = {
+                event: 'connection',
+                username,
+                id: Date.now()
+            }
+            socket.current.send(JSON.stringify(message))
+            console.log("Подключение установлено !!")
         }
 
-        socket.current.onmessage = () => {
-
+        socket.current.onmessage = (event) => {
+            const message = JSON.parse(event.data)
+            setMessages(prev => [message, ...prev])
         }
 
+        socket.current.onclose = () => {
+            console.log("Socket closed")
+        }
 
-
-    }, [])
+        socket.current.onerror = () => {
+            console.log("Socket problem")
+        }
+    }
 
     const sendMessage = async () => {
-        await axios.post('http://localhost:5000/new-messages', {
+        const message = {
+            username,
             message: value,
-            id: Date.now()
-        })
+            id: Date.now(),
+            event: 'message'
+        }
+        socket.current.send(JSON.stringify(message));
+        setValue('')
+    }
+
+    if (!connected) {
+        return (
+            <div className="center">
+                <div className="form">
+                    <input
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                        type="text"
+                        placeholder="Enter your name"/>
+                    <button onClick={connect}>Enter</button>
+                </div>
+            </div>
+        )
     }
 
     return (
@@ -37,8 +72,11 @@ const WebSockets = () => {
                 </div>
                 <div className="messages">
                     {messages.map(mess =>
-                        <div className="message" key={mess.id}>
-                            {mess.message}
+                        <div key={mess.id}>
+                            {mess.event === 'connection'
+                            ? <div className="connection_message">User {mess.username} подключился  </div>
+                            :  <div className="message">{mess.username}. {mess.message}</div>
+                            }
                         </div>
 
                     )}
